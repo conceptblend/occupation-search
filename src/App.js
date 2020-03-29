dd3a015ed1f753ac48c4cda2af50cfe2ef7e5e28
Andrew Wright <awright2@atb.com>
2020-03-28 22:16:07 -0600

    Added NAICS and generalized the approach

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Container, Header, Segment } from 'semantic-ui-react';
+import { Container, Header, Segment, Tab } from 'semantic-ui-react';
 import NocSearch from './components/Search';
+
+
+const panes = [
+  {
+    menuItem: 'Regular expression',
+    render: () => <Tab.Pane attached={false}><NocSearch key='0' method="regex"></NocSearch></Tab.Pane>,
+  },
+  {
+    menuItem: 'Fuzzy',
+    render: () => <Tab.Pane attached={false}><NocSearch key='1' method="fuzzy"></NocSearch></Tab.Pane>,
+  },
+]
 
 function App() {
   return (
     <Container>
       <Segment vertical>
-        <Header as="h1">National Occupation Code Explorer</Header>
-        <NocSearch></NocSearch>
+        <Header as="h1">Occupation Explorers</Header>
+      </Segment>
+      <Segment vertical>
+        <Tab renderActiveOnly={true} defaultActiveIndex={0} menu={{ secondary: true, pointing: true }} panes={panes} />
       </Segment>
     </Container>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 import PropTypes from 'prop-types'
+import Fuse from 'fuse.js'
 import { Search, Form, Card } from "semantic-ui-react";
 import _ from 'lodash'
-import { data as nocData } from '../data/noc-reduced.json'
+//import { data as nocData } from '../data/noc-reduced.json'
+import { data as nocData } from '../data/naics-reduced.json'
+
+const fuseOptions = {
+  shouldSort: true,
+  includeScore: true,
+  threshold: 0.4,
+  location: 0,
+  distance: 1000,
+  minMatchCharLength: 2,
+  keys: [
+    "nocTitle",
+    "combinedExamples"
+  ]
+};
 
 
 const resultRenderer = ({ title }) => [title && <div key='title'>{title}</div>];
 
 resultRenderer.propTypes = {
-  nocTitle: PropTypes.string,
-  nocCombinedExamples: PropTypes.string,
+  title: PropTypes.string,
 }
 
 const extendedOccupations = [
@@ -17,35 +31,37 @@ const extendedOccupations = [
     "nocTitle": "Not working",
     "nocLevel": "-1",
     "nocCode": "-1",
-    "nocCombinedExamples": ";unemployed;disability;"
+    "combinedExamples": ";unemployed;disability;"
   },
   {
     "nocTitle": "Retired",
     "nocLevel": "-1",
     "nocCode": "-1",
-    "nocCombinedExamples": ""
+    "combinedExamples": ""
   },
   {
     "nocTitle": "Student",
     "nocLevel": "-1",
     "nocCode": "-1",
-    "nocCombinedExamples": ""
+    "combinedExamples": ""
   },
   {
     "nocTitle": "Homemaker",
     "nocLevel": "-1",
     "nocCode": "-1",
-    "nocCombinedExamples": ";stay at home;mom;dad;parent;"
+    "combinedExamples": ";stay at home;mom;dad;parent;"
   },
 ];
 
 const source = nocData.concat(extendedOccupations);
+const fuse = new Fuse(source, fuseOptions); // "source" is the item array
+
 
 const NocSearch = (props) => {
   const [isLoading, setIsLoading] = useState(false);
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
-  const [selectedOccupation, setSelectedOccuaption] = useState({});
+  const [selectedOccupation, setSelectedOccupation] = useState({});
 
   const resetState = () => {
     setIsLoading(false);
@@ -55,7 +71,7 @@ const NocSearch = (props) => {
 
   const handleResultSelect = (e, { result }) =>  {
     setQuery(''); //result.nocTitle
-    setSelectedOccuaption(result);
+    setSelectedOccupation(result);
   }
 
   const handleSearchChange = (e, { value }) => {
@@ -66,16 +82,40 @@ const NocSearch = (props) => {
       if (value.length < 1) return resetState()
   
       const re = new RegExp(_.escapeRegExp(value), 'i')
-      const isMatch = (result) => (re.test(result.nocTitle) || re.test(result.nocCombinedExamples))
+      const isMatch = (result) => (re.test(result.nocTitle) || re.test(result.combinedExamples))
       const resultSort = (resultA, resultB) => (resultA.nocTitle).localeCompare(resultB.nocTitle)
 
       setIsLoading(false)
-      setResults(
-        _.map(
-          _.filter(source, isMatch).sort(resultSort),
-          result => { return { title: result.nocTitle }} // Mapping makes the consuming resultRender not dump all the props on the DOM
-        )
-      )
+
+      switch (props.method) {
+        case 'fuzzy':
+          setResults(
+            _.map(
+              fuse.search(value),
+              result => {return {
+                title: result.item.nocTitle,
+                description: result.item.combinedExamples,
+                meta: `Code ${result.item.nocCode} -- Level ${result.item.nocLevel}`,
+              }} // Mapping makes the consuming resultRender not dump all the props on the DOM
+            )
+          )
+          break;
+        case 'regex':
+          setResults(
+            _.map(
+              _.filter(source, isMatch).sort(resultSort),
+              result => {return {
+                title: result.nocTitle,
+                description: result.combinedExamples,
+                meta: `Code ${result.nocCode} -- Level ${result.nocLevel}`,
+              }} // Mapping makes the consuming resultRender not dump all the props on the DOM
+            )
+          )
+          break;
+        default:
+      }
+
+
     }, 300)
   }
   
@@ -100,16 +140,23 @@ const NocSearch = (props) => {
         />
       </Form.Field>
     </Form>
-    { selectedOccupation.nocTitle && 
+    { selectedOccupation.title && 
       <Card
         fluid
-        description={selectedOccupation.nocCombinedExamples}
-        header={selectedOccupation.nocTitle}
-        meta={`Code ${selectedOccupation.nocCode} -- Level ${selectedOccupation.nocLevel}`}
+        description={selectedOccupation.description}
+        header={selectedOccupation.title}
+        meta={selectedOccupation.meta}
       />
     }
     </>
   );
 };
+
+NocSearch.propTypes = {
+  method: PropTypes.oneOf(['fuzzy', 'regex']),
+}
+NocSearch.defaultProps = {
+  method: 'regex'
+}
 
 export default NocSearch;
